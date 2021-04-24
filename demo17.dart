7b865e6c2e7a927b11bc77f538cdb2121866f931
rabieAlarini <58493224+rabieAlarini@users.noreply.github.com>
2021-04-24 13:36:50 +0200

    Update demo17.dart

--- demo17.dart
+++ demo17.dart
@@ -11,3 +11,15 @@ main() {
 
 enum DaysOfWeek { sat, sun, mon, tue, wed, thu, fri }
 enum Colors { RED, GREEN, BLUE }
+// result
+/*
+DaysOfWeek.sun
+day DaysOfWeek.sat, index : 0
+day DaysOfWeek.sun, index : 1
+day DaysOfWeek.mon, index : 2
+day DaysOfWeek.tue, index : 3
+day DaysOfWeek.wed, index : 4
+day DaysOfWeek.thu, index : 5
+day DaysOfWeek.fri, index : 6
+Colors.RED
+*/
